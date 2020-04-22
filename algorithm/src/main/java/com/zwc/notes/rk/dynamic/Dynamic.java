a71de1bf3b9ef22077a6fff2f8a344fe7571e1c7
zhangwenchao19 <zhangwenchao19@jd.com>
2020-04-22 11:40:26 +0800

    jvm

---
+++ algorithm/src/main/java/com/zwc/notes/rk/dynamic/Dynamic.java
@@ -0,0 +1,15 @@
+package com.zwc.notes.rk.dynamic;
+
+/**
+ * 大事化小，小事化了
+ * <p>
+ * 题目：
+ * 有一座高度是10级台阶的楼梯，从下往上走，每跨一步只能向上1级或者2级台阶。要求用程序来求出一共有多少种走法。
+ */
+
+public class Dynamic {
+    public static void main(String[] args) {
+        // f(10)=f(9)+f(8)
+
+    }
+}
