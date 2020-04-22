2d19bcc71287a96844fcabca4f09cb7b7975d428
zhangwenchao19 <zhangwenchao19@jd.com>
2020-04-22 17:19:15 +0800

    jvm

--- algorithm/src/main/java/com/zwc/notes/rk/dynamic/DynamicKingGold.java
+++ algorithm/src/main/java/com/zwc/notes/rk/dynamic/DynamicKingGold.java
@@ -1,5 +1,7 @@
 package com.zwc.notes.rk.dynamic;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.HashMap;
 
 /**
@@ -14,9 +16,9 @@ import java.util.HashMap;
 public class DynamicKingGold {
     public static void main(String[] args) {
         int n = 5;
-        int w = 10;
-        int[] g = new int[]{400, 500, 200, 300, 350};
-        int[] p = new int[]{5, 5, 3, 4, 3};
+        int w = 18;
+        int[] g = new int[]{0, 400, 500, 200, 300, 350};
+        int[] p = new int[]{0, 5, 5, 3, 4, 3};
 
         int count = recursion(n, w, g, p);
         System.out.println("recursion:" + count);
@@ -25,8 +27,8 @@ public class DynamicKingGold {
         System.out.println("memorandum,map.size:" + map.size());
         System.out.println("memorandum:" + count2);
 
-         /*int count3 = dynamic(10);
-        System.out.println("dynamic:" + count3);*/
+        int count3 = dynamic(n, w, g, p);
+        System.out.println("dynamic:" + count3);
 
 
     }
@@ -42,11 +44,15 @@ public class DynamicKingGold {
         if (n < 1) {
             return 0;
         }
+        if (w <= 0) {
+            return 0;
+        }
+
         if (n == 1) {
-            if (w < p[0]) {
+            if (w < p[1]) {
                 return 0;
             } else {
-                return g[0];
+                return g[1];
             }
         }
 
@@ -59,10 +65,17 @@ public class DynamicKingGold {
          *
          *  F(n,w) = max(F(n-1,w),  F(n-1,w-p[n-1])+g[n-1])    (n>1, w>=p[n-1])
          */
-        int a = recursion(n - 1, w, g, p);
-        int b = recursion(n - 1, w - p[n - 1], g, p) + g[n - 1];
 
-        return a > b ? a : b;
+        // 工人总数少于当前金矿所需工人，所以无法开采
+        if (w < p[n]) {
+            return recursion(n - 1, w, g, p);
+        } else {
+            int a = recursion(n - 1, w, g, p);
+            int b = recursion(n - 1, w - p[n], g, p) + g[n];
+            int num = a > b ? a : b;
+            //System.out.println("n:"+n+"----w:" + w+"----num:" + num);
+            return num;
+        }
     }
 
     /**
@@ -79,6 +92,9 @@ public class DynamicKingGold {
         if (n < 1) {
             return 0;
         }
+        if (w <= 0) {
+            return 0;
+        }
         if (n == 1) {
             if (w < p[0]) {
                 return 0;
@@ -87,51 +103,83 @@ public class DynamicKingGold {
             }
         }
 
-        String keyA = n - 1 + "_" + w;
-        String keyB = n - 1 + "_" + (w - p[n - 1]);
-        int a;
-        int b;
-        if (map.containsKey(keyA)) {
-            a = map.get(keyA);
+        if (w < p[n - 1]) {
+            String key = n - 1 + "_" + w;
+            if (map.containsKey(key)) {
+                return map.get(key);
+            } else {
+                int num = memorandum(n - 1, w, g, p, map);
+                //System.out.println(keyA);
+                map.put(key, num);
+                return num;
+            }
         } else {
-            a = memorandum(n - 1, w, g, p, map);
-            //System.out.println(keyA);
-            map.put(keyA, a);
-        }
+            String keyA = n - 1 + "_" + w;
+            String keyB = n - 1 + "_" + (w - p[n - 1]);
+            int a;
+            int b;
+            if (map.containsKey(keyA)) {
+                a = map.get(keyA);
+            } else {
+                a = memorandum(n - 1, w, g, p, map);
+                //System.out.println(keyA);
+                map.put(keyA, a);
+            }
+            if (map.containsKey(keyB)) {
+                b = map.get(keyB);
+            } else {
+                b = memorandum(n - 1, w - p[n - 1], g, p, map) + g[n - 1];
+                map.put(keyB, b);
+                //System.out.println(keyB);
+            }
 
-        if (map.containsKey(keyB)) {
-            b = map.get(keyB);
-        } else {
-            b = memorandum(n - 1, w - p[n - 1], g, p, map) + g[n - 1];
-            map.put(keyB, a);
-            //System.out.println(keyB);
+            int num = a > b ? a : b;
+            //System.out.println("n:"+n+"----w:" + w+"----num:" + num);
+            return num;
         }
-
-        return a > b ? a : b;
     }
 
-    public static int dynamic(int n) {
-        if (n < 1) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        if (n == 2) {
-            return 2;
+    public static int dynamic(int n, int w, int[] g, int[] p) {
+        int[] preResults = new int[w + 1];
+        int[] results = new int[w + 1];
+
+        /**
+         *   F(n,w) = 0    (n==1, w<p[0]);
+         *
+         *   F(n,w) = g[0]     (n==1, w>=p[0]);
+         *
+         *  F(n,w) = F(n-1,w)    (n>1, w<p[n-1])
+         *
+         *  F(n,w) = max(F(n-1,w),  F(n-1,w-p[n-1])+g[n-1])    (n>1, w>=p[n-1])
+         */
+        // 初始化边界条件，n=1的情况
+        // n==1
+        for (int j = 1; j <= w; j++) {
+            // 当前工人数小于金矿所需功能
+            if (j < p[1]) {
+                preResults[j] = 0;
+            } else {
+                preResults[j] = g[1];
+            }
         }
 
-        int a = 1;
-        int b = 2;
-        int tmmp = 0;
-        for (int i = 3; i <= n; i++) {
-            tmmp = a + b;
-            a = b;
-            b = tmmp;
-
+        System.out.println("init preResults:" + JSON.toJSONString(preResults));
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                // 当前工人数小于金矿所需功能
+                // i==5,p[5]=3,g[5]=350,j=6
+                if (j < p[i]) {
+                    results[j] = preResults[j];
+                } else {
+                    int num = j - p[i];
+                    results[j] = Math.max(preResults[j], preResults[num] + g[i]);
+                }
+            }
+            System.arraycopy(results, 0, preResults, 0, results.length);
+            System.out.println("preResults:" + JSON.toJSONString(preResults));
         }
 
-        return tmmp;
+        return results[w];
     }
 
 }
