6d0694c453958fa964f2a7bc5844e08c46068b7f
zhangwenchao19 <zhangwenchao19@jd.com>
2020-08-07 17:38:54 +0800

    分布式算法

---
+++ algorithm/src/main/java/com/zwc/notes/rk/thread/Test1.java
@@ -0,0 +1,52 @@
+package com.zwc.notes.rk.thread;
+
+/**
+ *
+ */
+public class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new MyThread();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt(); // 中断t线程
+        t.join(); // 等待t线程结束
+        System.out.println("end");
+    }
+
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        Thread hello = new HelloThread();
+        hello.start(); // 启动hello线程
+        try {
+            hello.join(); // 等待hello线程结束
+        } catch (InterruptedException e) {
+            System.out.println("interrupted!");
+            // 表示当前线程被中断，此时中断HelloThread线程
+            hello.interrupt();
+        }
+
+    }
+}
+
+class HelloThread extends Thread {
+    @Override
+    public void run() {
+        int n = 0;
+        while (!isInterrupted()) {
+            n++;
+            System.out.println(n + " hello!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
+
+
+
+
