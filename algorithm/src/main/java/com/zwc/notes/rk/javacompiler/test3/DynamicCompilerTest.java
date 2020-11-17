0eb231f0ec22bf1c04cc41ca9577960586e781ba
zhangwenchao19 <zhangwenchao19@jd.com>
2020-11-17 22:26:18 +0800

    添加参数映射方法

--- algorithm/src/main/java/com/zwc/notes/rk/javacompiler/test3/DynamicCompilerTest.java
+++ algorithm/src/main/java/com/zwc/notes/rk/javacompiler/test3/DynamicCompilerTest.java
@@ -2,6 +2,7 @@ package com.zwc.notes.rk.javacompiler.test3;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 public class DynamicCompilerTest {
@@ -21,6 +22,12 @@ public class DynamicCompilerTest {
         Class clz = compiler.compileAndLoad(fullName, src.toString());
 
         System.out.println(clz.newInstance().getClass());
+        //获取方法
+        Method printMethod = clz.getMethod("toString");
+        //调用
+        Object vo = printMethod.invoke(clz.newInstance());
+        System.out.println(vo instanceof String);
+        System.out.println(vo);
         System.out.println(clz.getConstructor().newInstance());
         compiler.closeFileManager();
     }
