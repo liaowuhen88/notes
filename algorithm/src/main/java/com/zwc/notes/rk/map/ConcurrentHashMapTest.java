9583a6f88ccbbb1f485def8debd4c4c549ab43ba
zhangwenchao19 <zhangwenchao19@jd.com>
2020-12-23 21:03:03 +0800

    添加参数映射方法

--- algorithm/src/main/java/com/zwc/notes/rk/map/ConcurrentHashMapTest.java
+++ algorithm/src/main/java/com/zwc/notes/rk/map/ConcurrentHashMapTest.java
@@ -1,5 +1,6 @@
 package com.zwc.notes.rk.map;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -10,6 +11,23 @@ public class ConcurrentHashMapTest {
 
     public static void main(String[] args) {
 
+        test2();
+    }
+
+    private static void test2() {
+        System.out.println("--------------------");
+
+        HashMap map = new HashMap();
+
+        for (int i = 0; i < 30; i++) {
+            map.put("key" + i, "value" + i);
+        }
+
+
+        System.out.println(map.get("key20"));
+    }
+
+    private void test1() {
         System.out.println("--------------------");
 
         ConcurrentHashMap map = new ConcurrentHashMap();
